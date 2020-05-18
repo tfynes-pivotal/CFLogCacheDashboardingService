8fe3c3d0b1e563dbe5ace3c32dd42892304dd0b6
Mark Fynes <tfynes@pivotal.io>
2020-05-18 14:15:12 -0400

    .

--- setupGrafana.sh
+++ setupGrafana.sh
@@ -5,6 +5,7 @@ then
   mv ./grafana-6.7.3/bin .
   mv ./grafana-6.7.3/scripts .
   mv ./grafana-6.7.3/tools .
+  mv ./grafana-6.7.3/public .
 else
   echo "Please extract grafana binary to current director (assets land in ./grafana-6.7.3)"
   echo "    e.g. tar xvf ../../grafana-6.7.3.linux-amd64.tar.gz"
